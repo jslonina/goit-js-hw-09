9f2a1cd8cdb344aac0defd6dd34e7cefe3a2cc59
jslonina <99342408+jslonina@users.noreply.github.com>
2022-10-09 16:26:35 +0200

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const bodyCol = document.querySelector("body");
+let timerId;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function timer () {
+    timerId = setInterval(() => {
+       
+        bodyCol.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+  }
+
+  startBtn.addEventListener("click", () => {
+    console.log("start");
+    timer(getRandomHexColor);
+  })
+
+  stopBtn.addEventListener("click", () => {
+    console.log("stop");
+    clearInterval(timerId);
+  })
